a7cae82811e25106a03f0eb2342d163e1d410843
木兰 <13718047371m@sina.cn>
2019-09-23 13:07:58 +0800

    首页-路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/Login'
+import tabBar from '@/views/tabBar'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
@@ -8,6 +9,12 @@ const router = new VueRouter({
       name: 'login',
       path: '/login',
       component: Login
+
+    },
+    {
+      path: '/',
+      component: tabBar,
+      children: []
 
     }
   ]
---
+++ src/views/tabBar/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view />
+    <van-tabbar>
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签1</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabBarIndex'
+}
+</script>
+
+<style>
+
+</style>
